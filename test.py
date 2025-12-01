420cd8fc934f0b8c5181a3d8d6138aec23fc583e
Akshay-1999 <akshaypatil51316@gmail.com>
2025-12-01 16:47:15 +0530

    added the gitignore file and created test file for testing the syntax

--- test.py
+++ test.py
@@ -1,17 +1,9 @@
-from db.config import Database
+class Animal:
+    pass
 
-async def test_db_connection():
-    pool = await Database.get_pool()
-    async with pool.acquire() as connection:
-        if connection is None:
-            print("Failed to acquire a database connection.")
-        else:
-            print("Database connection acquired successfully.")
-            row = await connection.fetchrow("SELECT * FROM CORE.USERS LIMIT 1;")
-            print("Sample row from CORE.USERS:", row)
-        result = await connection.fetchval("SELECT 1;")
-        assert result == 1
+class Dog(Animal):
+    pass
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(test_db_connection())
+my_dog = Dog()
+print(isinstance(my_dog, Dog))    # Output: True
+print(isinstance(my_dog, Animal)) # Output: True
