3f5d96eb6a4b31dc426c8cef4da38acaf7721507
Akshay Patil <akpatil@estuate.com>
2026-04-09 17:29:30 +0530

    ssl for prod config db

--- db/config.py
+++ db/config.py
@@ -4,6 +4,7 @@ import os
 import asyncpg
 import json
 import asyncio
+import ssl
 
 load_dotenv()
 DB_USER = os.getenv("db_user")
@@ -11,6 +12,7 @@ DB_PASSWORD = os.getenv("db_password")
 DB_HOST = os.getenv("db_host")
 DB_PORT = os.getenv("db_port")
 DB_NAME = os.getenv("db_name")
+ENV = os.getenv("ENV")
 
 # print("Database URL:", DATABASE_URL)  # For debugging purposes only; remove in production
 
@@ -19,6 +21,11 @@ class Database:
     @classmethod
     async def get_pool(cls):
         if cls._pool is None:
+            use_ssl = False if ENV == "local" else True
+            ssl_context = None
+            if use_ssl:
+                ssl_context = ssl.create_default_context()
+                
             cls._pool = await asyncpg.create_pool(
                 user=DB_USER,
                 password=DB_PASSWORD,
@@ -27,6 +34,7 @@ class Database:
                 database=DB_NAME,
                 min_size=2,
                 max_size=10,
+                ssl=ssl_context
             )
         return cls._pool
     @classmethod
